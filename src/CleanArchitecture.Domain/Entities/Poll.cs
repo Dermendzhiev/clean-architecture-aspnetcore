9639c4c5b2f2043bc9deed188620a90c73cf68bf
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2019-11-17 12:42:43 +0200

    implement "SystemTime", instead of using DateTime.Now to improve testability

--- src/CleanArchitecture.Domain/Entities/Poll.cs
+++ src/CleanArchitecture.Domain/Entities/Poll.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using CleanArchitecture.Domain.SeedWork;
+    using CleanArchitecture.Domain.SharedKernel;
 
     public class Poll : Entity
     {
@@ -34,7 +35,7 @@
         {
             if (string.IsNullOrEmpty(title))
             {
-                throw new DomainException($"Title cannot be null or empty");
+                throw new DomainException("Title cannot be null or empty");
             }
 
             this.Title = title;
@@ -42,26 +43,34 @@
 
         public void SetDueDate(DateTime dueDate)
         {
-            if (dueDate <= DateTime.Now)
+            if (dueDate <= SystemTime.Now)
             {
-                throw new DomainException("Cannot set due date in the past!");
+                throw new DomainException("Cannot set due date in the past");
             }
 
             this.DueDate = dueDate;
         }
 
-        public void AddOption(string option) => this.options.Add(new Option(option));
+        public void AddOption(string option)
+        {
+            if (string.IsNullOrEmpty(option))
+            {
+                throw new DomainException("Option cannot be null or empty");
+            }
+
+            this.options.Add(new Option(option));
+        }
 
         public void Vote(string participantEmailAddress, ICollection<int> optionIds, DateTime votedAt)
         {
             if (this.SingleOptionLimitation && optionIds.Count > 1)
             {
-                throw new DomainException("Cannot vote for more than 1 options!");
+                throw new DomainException("Cannot vote for more than 1 options");
             }
 
-            if (this.DueDate <= DateTime.Now)
+            if (this.DueDate <= SystemTime.Now)
             {
-                throw new DomainException("Poll has expired!");
+                throw new DomainException("Poll has expired");
             }
 
             foreach (int optionId in optionIds)
---
+++ src/CleanArchitecture.Domain/SharedKernel/SystemTime.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArchitecture.Domain.SharedKernel
+{
+    using System;
+
+    public static class SystemTime
+    {
+        private static DateTime? customDate;
+
+        public static DateTime Now
+        {
+            get
+            {
+                if (customDate.HasValue)
+                {
+                    return customDate.Value;
+                }
+
+                return DateTime.UtcNow;
+            }
+        }
+
+        public static void Set(DateTime customDate) => SystemTime.customDate = customDate;
+
+        public static void Reset() => customDate = null;
+    }
+}
