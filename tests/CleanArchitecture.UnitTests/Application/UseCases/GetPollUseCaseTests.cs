86e46958c269e3cc357b7a285ff1b57a83a33b06
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2019-11-17 19:19:00 +0200

    add unit tests for "GetPollUseCase"

---
+++ tests/CleanArchitecture.UnitTests/Application/UseCases/GetPollUseCaseTests.cs
@@ -0,0 +1,116 @@
+﻿namespace CleanArchitecture.UnitTests.Application.UseCases
+{
+    using System;
+    using System.Threading.Tasks;
+    using CleanArchitecture.Application.Boundaries.GetPoll;
+    using CleanArchitecture.Application.Interfaces.Gateways;
+    using CleanArchitecture.Application.Interfaces.Infrastructure;
+    using CleanArchitecture.Application.UseCases;
+    using CleanArchitecture.Domain.Entities;
+    using CleanArchitecture.Domain.SeedWork;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Xunit;
+
+    public class GetPollUseCaseTests
+    {
+        [Fact]
+        public async Task HandleAsync_WhenPollExists_ShouldCallSuccess()
+        {
+            // Arrange
+            const int pollId = 1;
+
+            Poll poll = this.GetFakePoll();
+            poll.AddOption("test option");
+
+            var expectedGetPollOutput = new GetPollOutput(poll.Title, poll.Note, poll.DueDate, poll.SingleOptionLimitation, poll.Options);
+
+            IPollRepository pollRepositoryStub = A.Fake<IPollRepository>();
+            A.CallTo(() => pollRepositoryStub.GetAsync(pollId))
+                .Returns(poll);
+
+            IGetPollOutputBoundary getPollOutputBoundaryMock = A.Fake<IGetPollOutputBoundary>();
+
+            var useCase = new GetPollUseCase(null, pollRepositoryStub);
+
+            // Act
+            await useCase.HandleAsync(pollId, getPollOutputBoundaryMock);
+
+            // Assert
+            A.CallTo(
+                () => getPollOutputBoundaryMock.Success(
+                    A<GetPollOutput>.That.Matches(
+                        m => m.Title == poll.Title 
+                        && m.Note == poll.Note 
+                        && m.DueDate == poll.DueDate 
+                        && m.SingleOptionLimitation == poll.SingleOptionLimitation)))
+                .MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task HandleAsync_WhenPollDoesNotExist_ShouldCallNotFound()
+        {
+            // Arrange
+            const int pollId = 1;
+            const string expectedResultMessage = "Poll not found";
+
+            ILoggerService<GetPollUseCase> loggerServiceStub = A.Fake<ILoggerService<GetPollUseCase>>();
+            IPollRepository pollRepositoryStub = A.Fake<IPollRepository>();
+            A.CallTo(() => pollRepositoryStub.GetAsync(pollId))
+                .Returns((Poll)null);
+
+            IGetPollOutputBoundary getPollOutputBoundaryMock = A.Fake<IGetPollOutputBoundary>();
+
+            var useCase = new GetPollUseCase(loggerServiceStub, pollRepositoryStub);
+
+            // Act
+            await useCase.HandleAsync(1, getPollOutputBoundaryMock);
+
+            // Assert
+            A.CallTo(
+                () => getPollOutputBoundaryMock.NotFound(
+                    A<string>.That.Contains(expectedResultMessage)))
+                .MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task HandleAsync_WhenPollDoesNotExist_ShouldLogInformation()
+        {
+            // Arrange
+            const int pollId = 1;
+            const string expectedLogMessage = "Cannot retrieve a poll with {@id}";
+
+            IPollRepository pollRepositoryStub = A.Fake<IPollRepository>();
+            A.CallTo(() => pollRepositoryStub.GetAsync(pollId))
+                .Returns((Poll)null);
+
+            IGetPollOutputBoundary getPollOutputBoundaryStub = A.Fake<IGetPollOutputBoundary>();
+            ILoggerService<GetPollUseCase> loggerServiceMock = A.Fake<ILoggerService<GetPollUseCase>>();
+
+            var useCase = new GetPollUseCase(loggerServiceMock, pollRepositoryStub);
+
+            // Act
+            await useCase.HandleAsync(1, getPollOutputBoundaryStub);
+
+            // Assert
+            A.CallTo(
+                () => loggerServiceMock.LogInformation(
+                    A<string>.That.Contains(expectedLogMessage),
+                    A<int>.That.IsEqualTo(pollId)))
+                .MustHaveHappened();
+        }
+
+        private Poll GetFakePoll(bool? singleOptionLimitation = true, DateTime? dueDate = default)
+        {
+            const string title = "fake title";
+            const string note = "fake note";
+
+            if (dueDate == default)
+            {
+                dueDate = DateTime.Now.AddDays(1);
+            }
+
+            return new Poll(title, note, singleOptionLimitation.Value, dueDate.Value);
+        }
+    }
+}
